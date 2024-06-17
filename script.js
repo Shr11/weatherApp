aa058f2116ed416a1dc9b30f60060c73c629708a
Shraddha_Sh <shr.comp1@gmail.com>
2024-06-18 01:43:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let valueSearch = document.getElementById('valueSearch')
+let city = document.getElementById('city')
+let temperature = document.querySelector('.temperature')
+let description = document.querySelector('.description')
+let clouds = document.getElementById('clouds')
+let humidity = document.getElementById('humidity')
+let pressure = document.getElementById('pressure')
+let form = document.querySelector('form')
+form.addEventListener('submit', (event) => {
+    alert('submitted');
+})
